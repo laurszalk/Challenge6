f504a4304d07f9cddac7598977e19568bfa5837e
laurszalk <laurenszalk@gmail.com>
2023-01-11 13:24:12 -0500

    got the cities to display on the screen

--- assets/script.js
+++ assets/script.js
@@ -22,11 +22,24 @@ $(function () {
 
   //click event on button to display the previously searched cities on the webapge
   //need to capture the input text and display on page
-  button.click(function () {
-    var cityListItem = $("#city-input").val();
-    $("form").append("<input />").attr("value", cityListItem);
+  // button.click(function () {
+  //   var cityListItem = $("#city-input").val();
+  //   $("form").append("<input />").attr("value", cityListItem);
 
-   
-    // I tried to use formData.append("value", cityListItem) but it didn't work;
+  button.click(function () {
+    // get the city name
+    var cityListItem = $("#city-input").val();
+
+    // create a new input element and set attributes
+    const newInput = $("<input>")
+      .attr("value", cityListItem)
+      .attr("type", "submit")
+      .attr("class", "form-control shadow-sm");
+
+    // now append the new element to a parent element (here it's the form)
+    $("form").append(newInput);
+
+    //clear the form after you type a city
+    $("#city-input").val("");
   });
 });
