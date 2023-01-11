b8196279d3fa65b968cb893c4e34cb26cb40cb65
laurszalk <laurenszalk@gmail.com>
2023-01-10 19:50:15 -0500

    added display time function

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
-let apiKey = "a64546395beb5601577c4fae1c60f311";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+// let apiKey = "a64546395beb5601577c4fae1c60f311";
+// let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+//displays current day and time at the top of page
+var timeDisplay = $("#currentDay");
+function displayTime() {
+  var rightNow = dayjs().format("dddd, MMM DD, YYYY");
+  timeDisplay.text(rightNow);
+}
+
+displayTime();
