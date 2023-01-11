9941512189e6a0b788a7c3aef9c6e6891044ace3
laurszalk <laurenszalk@gmail.com>
2023-01-11 12:31:37 -0500

    adding click event that displays recent searches on page

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,31 @@
 // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 //displays current day and time at the top of page
-var timeDisplay = $("#currentDay");
-function displayTime() {
-  var rightNow = dayjs().format("dddd, MMM DD, YYYY");
-  timeDisplay.text(rightNow);
-}
+var button = $(".btn");
 
-displayTime();
+$(function () {
+  var timeDisplay = $("#currentDay");
+  function displayTime() {
+    var rightNow = dayjs().format("dddd, MMM DD, YYYY");
+    timeDisplay.text(rightNow);
+  }
 
-var button = $("#btn");
+  displayTime();
 
-button.click(function () {
-  console.log("Does this work?");
+  //i need to add the fetch I think onto this click event
+
+  button.click(function (event) {
+    event.preventDefault();
+    console.log("Does this work?");
+  });
+
+  //click event on button to display the previously searched cities on the webapge
+  //need to capture the input text and display on page
+  button.click(function () {
+    var cityListItem = $("#city-input").val();
+    $("form").append("<input />").attr("value", cityListItem);
+
+   
+    // I tried to use formData.append("value", cityListItem) but it didn't work;
+  });
 });
