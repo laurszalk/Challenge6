4fa04149dd0cb89025bc23ec023bbdfb6e155589
laurszalk <laurenszalk@gmail.com>
2023-04-15 18:21:11 -0400

    rendered history button to the page

--- assets/app.js
+++ assets/app.js
@@ -3,16 +3,17 @@ var apiKey = "a64546395beb5601577c4fae1c60f311";
 //   "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" +
 //   apiKey;
 
-var cityEl = document.querySelector("#current-city").value;
+var cityInput = document.querySelector("#city-input");
 var temperatureEl = document.querySelector("#temperature");
 var descriptionEl = document.querySelector("#description");
 var windEl = document.querySelector("#wind-speed");
 var humidityEl = document.querySelector("#humidity");
 var iconEl = document.querySelector("#current-icon");
 var searchForm = document.querySelector("#form");
+var currentCityEl = document.querySelector("#current-city");
 
 function getWeather() {
-  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityEl}&units=imperial&appid=${apiKey}`;
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=imperial&appid=${apiKey}`;
   fetch(apiUrl)
     .then(function (response) {
       if (response.status === 200) {
@@ -24,7 +25,7 @@ function getWeather() {
     .then(function (data) {
       console.log(data);
 
-      cityEl.textContent = data.name;
+      currentCityEl.textContent = data.name;
       temperatureEl.innerHTML = Math.round(data.main.temp) + "°F";
       descriptionEl.innerHTML = data.weather[0].description;
       windEl.textContent = "Wind: " + data.wind.speed + " MPH";
@@ -38,8 +39,19 @@ function getWeather() {
 
 function handleFormSubmit(event) {
   event.preventDefault();
-  var cityInputEl = document.querySelector("#city-input");
-  getWeather(cityInputEl);
+  if (cityInput.value === "") {
+    alert("Please enter a city");
+  } else {
+    var historyButton = document.createElement("button");
+    historyButton.innerHTML = cityInput.value;
+    historyButton.setAttribute("class", "w-100 btn btn-primary");
+    // // now append the new element to a parent element (here it's the form)
+    searchForm.append(historyButton);
+
+    getWeather(cityInput);
+  }
+  //clear the form after you type a city
+  $("#city-input").val("");
 }
 
 //displays current day at the top of page
@@ -53,3 +65,5 @@ displayTime();
 
 //search a city when you click the search button
 searchForm.addEventListener("submit", handleFormSubmit);
+
+//need to add when you click history button
