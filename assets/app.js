27837ad1aadb49a0130db66ed9bcf64da7b2a039
laurszalk <laurenszalk@gmail.com>
2023-04-15 16:52:20 -0400

    handle form submit function and rewriting fetch function

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,20 @@
 var apiKey = "a64546395beb5601577c4fae1c60f311";
-var apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" +
-  apiKey;
+// var apiUrl =
+//   "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" +
+//   apiKey;
 
 var cityEl = document.querySelector("#current-city");
 var temperatureEl = document.querySelector("#temperature");
 var descriptionEl = document.querySelector("#description");
 var windEl = document.querySelector("#wind-speed");
 var humidityEl = document.querySelector("#humidity");
+var iconEl = document.querySelector("#current-icon");
 var searchForm = document.querySelector("#form");
 
 function getWeather() {
+  // var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=" + ${city} + "&units=imperial&appid=" +
+  // apiKey`;
+
   fetch(apiUrl)
     .then(function (response) {
       if (response.status === 200) {
@@ -27,10 +31,27 @@ function getWeather() {
       descriptionEl.innerHTML = data.weather[0].description;
       windEl.textContent = "Wind: " + data.wind.speed + " MPH";
       humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
+      iconEl.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+      );
     });
 }
 
-getWeather();
+// getWeather();
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  // var cityInputEl = document.querySelector("#city-input");
+  var city = $("#city-input").val();
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial&appid=" +
+    apiKey;
+
+  getWeather(apiUrl);
+}
 
 //displays current day at the top of page
 var timeDisplay = $("#currentDay");
@@ -42,13 +63,4 @@ function displayTime() {
 displayTime();
 
 //search a city when you click the search button
-searchForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var city = $("#city-input").val();
-
-  var apiUrl =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    city +
-    "&units=imperial&appid=" +
-    apiKey;
-});
+searchForm.addEventListener("submit", handleFormSubmit);
--- assets/script.js
+++ assets/script.js
@@ -1,128 +1,124 @@
-var apiKey = "a64546395beb5601577c4fae1c60f311";
-var searchForm = $("#form");
-var weatherDisplay = document.getElementById("five-day-forecast");
-var apiUrl =
-  "https://api.openweathermap.org/data/2.5/forecast?q=" +
-  city +
-  "&units=imperial&appid=" +
-  apiKey;
+// var apiKey = "a64546395beb5601577c4fae1c60f311";
+// var searchForm = $("#form");
+// var weatherDisplay = document.getElementById("five-day-forecast");
+// var apiUrl =
+//   "https://api.openweathermap.org/data/2.5/forecast?q=" +
+//   city +
+//   "&units=imperial&appid=" +
+//   apiKey;
 
-//displays current day at the top of page
-var timeDisplay = $("#currentDay");
-function displayTime() {
-  var rightNow = dayjs().format("dddd, MMM DD, YYYY");
-  timeDisplay.text(rightNow);
-}
+// searchForm.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   var city = $("#city-input").val();
 
-displayTime();
+//   var apiUrl =
+//     "https://api.openweathermap.org/data/2.5/forecast?q=" +
+//     city +
+//     "&units=imperial&appid=" +
+//     apiKey;
 
-searchForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  var city = $("#city-input").val();
+//   getWeather(apiUrl);
 
-  var apiUrl =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    city +
-    "&units=imperial&appid=" +
-    apiKey;
+//   //the event handler submit is what's creating the button and then we attach the event to it
+//   // get the city name
+//   var cityListItem = $("#city-input").val();
+//   if (cityListItem === "") {
+//     alert("Please enter a city");
+//   } else {
+//     // create a new button element and set attributes
+//     var historyButton = $(`<button>${cityListItem}</button>`) //using template literals in place of concatenation
+//       .attr("type", "button")
+//       .attr("class", "w-100 btn btn-primary");
+//     // display the weather dashboard
+//     weatherDisplay.classList.remove("hide");
+//   }
+//   // now append the new element to a parent element (here it's the form)
+//   searchForm.append(historyButton);
 
-  getWeather(apiUrl);
+//   //clear the form after you type a city
+//   $("#city-input").val("");
 
-  //the event handler submit is what's creating the button and then we attach the event to it
-  // get the city name
-  var cityListItem = $("#city-input").val();
-  if (cityListItem === "") {
-    alert("Please enter a city");
-  } else {
-    // create a new button element and set attributes
-    var historyButton = $(`<button>${cityListItem}</button>`) //using template literals in place of concatenation
-      .attr("type", "button")
-      .attr("class", "w-100 btn btn-primary");
-    // display the weather dashboard
-    weatherDisplay.classList.remove("hide");
-  }
-  // now append the new element to a parent element (here it's the form)
-  searchForm.append(historyButton);
+//   //add click event to the newly appended city
+//   //so that we can click the city and get the weather
+//   historyButton.on("click", function (event) {
+//     cityListItem = event.target.innerText;
+//     event.preventDefault();
+//     console.log(cityListItem);
+//     $("#city-input").val(cityListItem);
+//     forecastQueryURL =
+//       "https://api.openweathermap.org/data/2.5/forecast?q=" +
+//       cityListItem +
+//       "&units=imperial&appid=" +
+//       apiKey;
 
-  //clear the form after you type a city
-  $("#city-input").val("");
+//     getWeather(apiUrl);
+//   });
+// });
 
-  //add click event to the newly appended city
-  //so that we can click the city and get the weather
-  historyButton.on("click", function (event) {
-    cityListItem = event.target.innerText;
-    event.preventDefault();
-    console.log(cityListItem);
-    $("#city-input").val(cityListItem);
-    forecastQueryURL =
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      cityListItem +
-      "&units=imperial&appid=" +
-      apiKey;
+// function getWeather(apiUrl) {
+//   //the forecast queryUrl is the endpoint for the api
+//   //browser only understands string data types (JSON and javascript)
+//   //fetch is built into the browser for us
+//   //response is a promise, we don't know how long it will take to fulfill
+//   fetch(apiUrl)
+//     //callback function, we call it after some other operation happens
+//     .then(function (response) {
+//       console.log(response);
 
-    getWeather(apiUrl);
-  });
-});
+//       //  Conditional for the the response.status; checking to make sure the status is good
+//       if (response.status === 200) {
+//         //need to pass this information to the next .then
+//         //the first .then needs to complete before the next .then
+//         //.json() returns the json object and converts it to javascript object
+//         //in json the keys and values are both strings
+//         return response.json();
+//       } else {
+//         // if the status is not 200, then we throw an error
+//         throw new Error("Something went wrong");
+//       }
+//     })
+//     //the response object in javascript format from line 76
+//     //we need to pull out the info we need from data- data is our js object
+//     .then(function (data) {
+//       console.log(data);
+//       var lat = data.city.coord.lat;
+//       var lon = data.city.coord.lon;
+//       var currentWeatherUrl =
+//         "https://api.openweathermap.org/data/2.5/weather?lat=" +
+//         lat +
+//         "&lon=" +
+//         lon +
+//         "&appid=" +
+//         apiKey;
 
-function getWeather(apiUrl) {
-  //the forecast queryUrl is the endpoint for the api
-  //browser only understands string data types (JSON and javascript)
-  //fetch is built into the browser for us
-  //response is a promise, we don't know how long it will take to fulfill
-  fetch(apiUrl)
-    //callback function, we call it after some other operation happens
-    .then(function (response) {
-      console.log(response);
+//       console.log(lat);
+//       console.log(lon);
 
-      //  Conditional for the the response.status; checking to make sure the status is good
-      if (response.status === 200) {
-        //need to pass this information to the next .then
-        //the first .then needs to complete before the next .then
-        //.json() returns the json object and converts it to javascript object
-        //in json the keys and values are both strings
-        return response.json();
-      } else {
-        // if the status is not 200, then we throw an error
-        throw new Error("Something went wrong");
-      }
-    })
-    //the response object in javascript format from line 76
-    //we need to pull out the info we need from data- data is our js object
-    .then(function (data) {
-      console.log(data);
-      var lat = data.city.coord.lat;
-      var lon = data.city.coord.lon;
-      var currentWeatherUrl =
-        "https://api.openweathermap.org/data/2.5/weather?lat=" +
-        lat +
-        "&lon=" +
-        lon +
-        "&appid=" +
-        apiKey;
+//       fetch(currentWeatherUrl).then(function (response) {
+//         console.log(response);
 
-      console.log(lat);
-      console.log(lon);
+//         if (response.status === 200) {
+//           //need to pass this information to the next .then
+//           //the first .then needs to complete before the next .then
+//           //.json() returns the json object and converts it to javascript object
+//           //in json the keys and values are both strings
+//           return response.json();
+//         } else {
+//           throw new Error("Something went wrong");
+//         }
+//       });
+//     });
+//   console.log("i am after the fetch request");
+// }
 
-      fetch(currentWeatherUrl).then(function (response) {
-        console.log(response);
+// //displays current day at the top of page
+// var timeDisplay = $("#currentDay");
+// function displayTime() {
+//   var rightNow = dayjs().format("dddd, MMM DD, YYYY");
+//   timeDisplay.text(rightNow);
+// }
 
-        //  Conditional for the the response.status; checking to make sure the status is good
-        if (response.status === 200) {
-          //need to pass this information to the next .then
-          //the first .then needs to complete before the next .then
-          //.json() returns the json object and converts it to javascript object
-          //in json the keys and values are both strings
-          return response.json();
-        } else {
-          // if the status is not 200, then we throw an error
-          throw new Error("Something went wrong");
-        }
-      });
-    });
-  console.log("i am after the fetch request");
-}
+// displayTime();
 
-//will need to store and retrieve to local storage can do it last
-//take results of data and write it to forecast
-//need to add ids to html
-//remember i+=7 for results
+// //will need to store and retrieve to local storage can do it last
+// //remember i+=7 for results
